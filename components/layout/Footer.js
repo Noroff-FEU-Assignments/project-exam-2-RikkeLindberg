10fc764806215b267552f6d9bda3db51770bb309
RikkeLindberg <lindbergxrikke@gmail.com>
2021-04-22 10:00:53 +0200

    add class names

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,9 +1,35 @@
 import styles from './Footer.module.css';
+import Link from 'next/link';
 
 export default function Footer() {
     return (
-        <footer>
-            <small>Project exame</small>
+        <footer className={styles.footer}>
+            <Link href="/"><a className={styles.logo}>Holidaze</a></Link>
+
+            <ul className={styles.list}>
+                <li>
+                    <Link href="/">
+                        <a className={styles.link}>Home</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/hotels">
+                        <a className={styles.link}>Hotels</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/contact">
+                        <a className={styles.link}>Contact</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/login">
+                        <a className={styles.link}>Login</a>
+                    </Link>
+                </li>
+            </ul>
+
+            <small className={styles.copyright}>© 2021 Holidaze, Rikke Lindberg - Project Exam 2</small>
         </footer>
     )
 }
