ff39f1c9cd11a5e1fb42bda73765435f23154a3a
RikkeLindberg <lindbergxrikke@gmail.com>
2021-04-22 10:00:09 +0200

    change class names

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -10,41 +10,39 @@ export default function Navbar() {
     } 
 
     return (
-        <header className={styles.header}>
             <nav className={styles.nav}>
-                <Link href="/"><a className={styles.navlogo}>Holidaze</a></Link>
+                <Link href="/"><a className={styles.logo}>Holidaze</a></Link>
 
-                <ul className={toggled === false ? styles.nav_list : `${styles.nav_list} ${styles.active}`}>
-                    <li className={styles.nav_item} onClick={() => handleToggle()}>
+                <ul className={toggled === false ? styles.list : `${styles.list} ${styles.active}`}>
+                    <li className={styles.item} onClick={() => handleToggle()}>
                         <Link href="/">
-                            <a className={styles.nav_link}>Home</a>
+                            <a className={styles.link}>Home</a>
                         </Link>
                     </li>
-                    <li className={styles.nav_item} onClick={() => handleToggle()}>
+                    <li className={styles.item} onClick={() => handleToggle()}>
                         <Link href="/hotels">
-                            <a className={styles.nav_link}>Hotels</a>
+                            <a className={styles.link}>Hotels</a>
                         </Link>
                     </li>
-                    <li className={styles.nav_item} onClick={() => handleToggle()}>
+                    <li className={styles.item} onClick={() => handleToggle()}>
                         <Link href="/contact">
-                            <a className={styles.nav_link}>Contact</a>
+                            <a className={styles.link}>Contact</a>
                         </Link>
                     </li>
-                    <li className={styles.nav_item} onClick={() => handleToggle()}>
+                    <li className={styles.item} onClick={() => handleToggle()}>
                         <Link href="/login">
-                            <a className={styles.nav_btn}>Login</a>
+                            <a className={styles.btn}>Login</a>
                         </Link>
                     </li>
                 </ul>
 
                 <button className={toggled === false ? styles.toggle : `${styles.toggle} ${styles.active}`} onClick={() => handleToggle()}>
-                    <span className={styles.toggle_bar}></span>
-                    <span className={styles.toggle_bar}></span>
-                    <span className={styles.toggle_bar}></span>
+                    <span className={styles.bar}></span>
+                    <span className={styles.bar}></span>
+                    <span className={styles.bar}></span>
                 </button>
 
-                <Link href="/login"><a className={styles.nav_btn}>Login</a></Link>
+                <Link href="/login"><a className={styles.btn}>Login</a></Link>
             </nav>
-        </header>
     )
 }
