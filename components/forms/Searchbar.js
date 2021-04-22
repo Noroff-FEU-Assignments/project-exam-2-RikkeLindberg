31c9db9ac66c46ac26150ea944c3ba5760eaf7d3
RikkeLindberg <lindbergxrikke@gmail.com>
2021-04-22 15:39:55 +0200

    add styles to input

---
+++ components/forms/Searchbar.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import styles from './Searchbar.module.css';
+
+export default function Searchbar({ type, text }) {
+    return (
+        <form className={styles.form}>
+            <input
+                type={ type }
+                placeholder={ text }
+                className={styles.input}
+            />
+        </form>
+    )
+}
+
+Searchbar.propTypes = {
+	type: PropTypes.string.isRequired,
+    placeholder: PropTypes.string.isRequired,
+};
