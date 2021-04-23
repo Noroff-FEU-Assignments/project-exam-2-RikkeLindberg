38ffb085d77176753ce7b00f6d87e93e408932b4
RikkeLindberg <lindbergxrikke@gmail.com>
2021-04-23 19:12:44 +0200

    chnage paragraph content

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -7,8 +7,7 @@ export default function Hero() {
     return (
         <div className={styles.hero}>
             <Heading size="1" title="Tourism Agency in Bergen"/>
-            <Paragraph content="Visit Bergen and find the way you want to live on vacation either it is in a hotel, 
-B&B or a guesthouse we got you covered.  So start your vacation here at Holidaze."/>
+            <Paragraph content="Visit Bergen and find the way you want to live on vacation either it is in a hotel, B&B or a guesthouse we got you covered."/>
             <Button link="/hotels">See all hotels</Button>
         </div>
     )
