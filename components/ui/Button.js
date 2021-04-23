392cf543146de3466643926a1c3aee1cd33a9692
RikkeLindberg <lindbergxrikke@gmail.com>
2021-04-24 01:27:41 +0200

    remove Link

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -1,20 +1,17 @@
 import PropTypes from 'prop-types';
-import Link from 'next/link';
 import styles from './Button.module.css';
 
 export default function Button({ link, children }) {
     return (
         <button className={styles.button}>
-            <Link href={ link }>
-                <a>
-                    { children }
-                </a>
-            </Link>
+            <a href={ link }>
+                { children }
+            </a>
         </button>
     )
 }
 
 Button.propTypes = {
     children: PropTypes.string.isRequired,
-	link: PropTypes.string.isRequired,
+	link: PropTypes.string,
 };
