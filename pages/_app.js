098c92295038c2586dceb78b01e2cd3fc721040c
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-06 10:06:47 +0200

    import Authorisation Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
+import { AuthorisationProvider } from "../context/AuthorisationContext";
 import Layout from '../components/layout/Layout';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AuthorisationProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AuthorisationProvider>
   )
 }
 
