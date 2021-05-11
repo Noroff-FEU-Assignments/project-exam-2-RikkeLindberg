4f8e7eb7652393be11537bee4c843a47a43ca78c
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-11 14:36:55 +0200

    fetch and display data from api

---
+++ pages/establishments.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import PropTypes from "prop-types";
+import Heading from "../components/typography/Heading";
+import EstablishmentList from '../components/establishments/EstablishmentList';
+import { BASE_URL } from "../constants/api";
+
+export default function Establishments({ results }) {
+    console.log(results);
+
+    return (
+        <>
+            <Heading size="1" title="Establishments"/>
+            <EstablishmentList results={ results }/>
+        </>
+    )
+}
+
+Establishments.propTypes = {
+    results: PropTypes.arrayOf(PropTypes.object),
+};
+
+const url = BASE_URL + "establishments";
+
+export async function getStaticProps() {
+    let results = [];
+  
+    try {
+      const response = await axios.get(url);
+  
+      results = response.data;
+  
+    } catch (error) {
+      console.log(error);
+    }
+  
+    return {
+          props: {
+              results: results,
+          },
+      };
+  }
