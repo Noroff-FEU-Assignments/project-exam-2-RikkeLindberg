37c1cc7f8cc3d50e095e0c82db7abdf366f8dde1
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-11 14:41:56 +0200

    remove fragment

--- components/home/Explore.js
+++ components/home/Explore.js
@@ -7,18 +7,20 @@ export default function Explore() {
         <section>
             <Heading size="2" title="Explore bergen" />
             <CardsGrid>
-                <Card src="/img/natours.JPG" alt="bergen" title="Fløybanen" content="Lorem ipsum dolor sit amet, consectetur 
-                    adipiscing elit, sed do eiusmod tempor 
-                    incididunt ut labore et dolore magna 
-                    aliqua. Ut enim ad minim veniam, quis" link="https://www.visitbergen.com/" text="learn more" />
-                <Card src="/img/natours.JPG" alt="bergen" title="Fløven" content="Lorem ipsum dolor sit amet, consectetur 
-                    adipiscing elit, sed do eiusmod tempor 
-                    incididunt ut labore." link="https://www.visitbergen.com/" text="learn more" />
-                <Card src="/img/natours.JPG" alt="bergen" title="Bergen brygge" content="Lorem ipsum dolor sit amet, consectetur 
-                    adipiscing elit, sed do eiusmod tempor 
-                    incididunt ut labore et dolore magna" link="https://www.visitbergen.com/" text="learn more" />
-                <Card src="/img/natours.JPG" alt="bergen" title="Akvarium" content="Lorem ipsum dolor sit amet, consectetur 
-                    adipiscing elit, sed do eiusmod tempor" link="https://www.visitbergen.com/" text="learn more" />
+                <>
+                    <Card src="/img/natours.JPG" alt="bergen" title="Fløybanen" content="Lorem ipsum dolor sit amet, consectetur 
+                        adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna 
+                        aliqua. Ut enim ad minim veniam, quis" link="https://www.visitbergen.com/" text="learn more" />
+                    <Card src="/img/natours.JPG" alt="bergen" title="Fløven" content="Lorem ipsum dolor sit amet, consectetur 
+                        adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore." link="https://www.visitbergen.com/" text="learn more" />
+                    <Card src="/img/natours.JPG" alt="bergen" title="Bergen brygge" content="Lorem ipsum dolor sit amet, consectetur 
+                        adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna" link="https://www.visitbergen.com/" text="learn more" />
+                    <Card src="/img/natours.JPG" alt="bergen" title="Akvarium" content="Lorem ipsum dolor sit amet, consectetur 
+                        adipiscing elit, sed do eiusmod tempor" link="https://www.visitbergen.com/" text="learn more" />
+                </>
             </ CardsGrid>
         </section>
     )
