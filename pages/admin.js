18a44afeb847c5da259b3bc9697a3a915c9ba4ce
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-12 11:01:12 +0200

    Import Enquiries and Messages

--- pages/admin.js
+++ pages/admin.js
@@ -1,7 +1,13 @@
-export default function admin() {
+import Heading from "../components/typography/Heading";
+import Enquiries from "../components/admin/Enquiries";
+import Messages from "../components/admin/Messages";
+
+export default function Admin() {
     return (
         <div>
-            Admin
+            <Heading size="1" title="Admin"/>
+            <Messages />
+            <Enquiries />
         </div>
     )
 }
