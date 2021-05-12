3f0c069e1e51632fcdfb60faa90d0033bdd13b2c
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-12 15:05:17 +0200

    add Button and link to create new establishment

--- pages/admin.js
+++ pages/admin.js
@@ -1,13 +1,25 @@
-import Heading from "../components/typography/Heading";
-import Enquiries from "../components/admin/Enquiries";
-import Messages from "../components/admin/Messages";
+import { useContext } from "react";
+import Heading from '../components/typography/Heading'
+import Enquiries from '../components/admin/Enquiries'
+import Messages from '../components/admin/Messages'
+import Button from '../components/ui/Button'
+import AuthContext from "../context/AuthContext";
 
 export default function Admin() {
+
+    const [auth] = useContext(AuthContext);
+
     return (
-        <div>
-            <Heading size="1" title="Admin"/>
-            <Messages />
-            <Enquiries />
-        </div>
+        <>
+            { auth ? (
+                <div>
+                    <Heading size="1" title="Admin"/>
+                    <Button link="/createNew">Create New</Button>
+                    <Messages />
+                    <Enquiries />
+                </div>
+                ) : ( <div>NOT ALLOWED</div> )
+            }
+        </>
     )
 }
