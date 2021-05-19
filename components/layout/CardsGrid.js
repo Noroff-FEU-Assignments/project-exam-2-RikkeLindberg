34c6ccbc84ac82e43bf822de33ffb7b237b884c1
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-19 15:31:56 +0200

    format code

--- components/layout/CardsGrid.js
+++ components/layout/CardsGrid.js
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types';
-import styles from './CardsGrid.module.css';
+import PropTypes from 'prop-types'
+import styles from './CardsGrid.module.css'
 
 export default function CardsGrid({ children }) {
     return (
