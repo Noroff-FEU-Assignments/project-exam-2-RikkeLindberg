16e1a1db19ad32513ced3abc39cf1a019e59642e
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-19 10:53:57 +0200

    remove toggle and change layout

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -1,67 +1,61 @@
-import { useState } from 'react';
-import { useContext } from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import AuthContext from "../../context/AuthContext";
-import styles from './Navbar.module.css';
+import { BiHomeAlt, BiUser, BiBuildingHouse, BiEnvelope } from 'react-icons/bi';
+import { useContext } from 'react'
+import Link from 'next/link'
+import AuthContext from '../../context/AuthContext'
+import styles from './Navbar.module.css'
 
 export default function Navbar() {
-
-    const [toggled, setToggled] = useState(false);
-    const handleToggle = () => {
-       setToggled(!toggled); 
-    }
-
-    const [auth, setAuth] = useContext(AuthContext);
-
-    const router = useRouter();
-
-    function logout() {
-        setAuth(null);
-        router.push("/login");
-    }
+    const [auth] = useContext(AuthContext);
 
     return (
+        <>
+            <Link href="/">
+                <a className={styles.logo}>Holidaze</a>
+            </Link>
+            
             <nav className={styles.nav}>
-                <Link href="/"><a className={styles.logo}>Holidaze</a></Link>
-
-                <ul className={toggled === false ? styles.list : `${styles.list} ${styles.active}`}>
-                    <li className={styles.item} onClick={() => handleToggle()}>
+                <ul className={styles.list}>
+                    <li className={styles.item}>
                         <Link href="/">
-                            <a className={styles.link}>Home</a>
+                            <a className={styles.link}>
+                                <BiHomeAlt className={styles.icon} /> Home
+                            </a>
                         </Link>
                     </li>
-                    <li className={styles.item} onClick={() => handleToggle()}>
+                    <li className={styles.item}>
                         <Link href="/establishments">
-                            <a className={styles.link}>Establishments</a>
+                            <a className={styles.link}>
+                                <BiBuildingHouse className={styles.icon} /> Hotels
+                            </a>
                         </Link>
                     </li>
-                    <li className={styles.item} onClick={() => handleToggle()}>
+                    <li className={styles.item}>
                         <Link href="/contact">
-                            <a className={styles.link}>Contact</a>
+                            <a className={styles.link}>
+                                <BiEnvelope className={styles.icon} /> Contact
+                            </a>
                         </Link>
                     </li>
 
                     { auth ? (
-                        <li className={styles.item} onClick={() => handleToggle()}>
+                        <li className={styles.item}>
                             <Link href="/admin">
-                                <a className={styles.link}>Admin</a>
+                                <a className={styles.link}>
+                                    <BiUser className={styles.icon} /> Admin
+                                </a>
                             </Link>
-                            <button onClick={logout}>Log out</button>
                         </li> 
-                    ) : (   <li className={styles.item} onClick={() => handleToggle()}>
-                                <Link href="/login">
-                                    <a className={styles.btn}>Login</a>
-                                </Link>
-                            </li> ) 
+                    ) : (
+                        <li className={styles.item}>
+                            <Link href="/login">
+                                <a className={styles.link}>
+                                    <BiUser className={styles.icon} /> Login
+                                </a>
+                            </Link>
+                        </li> ) 
                     } 
                 </ul>
-
-                <button className={toggled === false ? styles.toggle : `${styles.toggle} ${styles.active}`} onClick={() => handleToggle()}>
-                    <span className={styles.bar}></span>
-                    <span className={styles.bar}></span>
-                    <span className={styles.bar}></span>
-                </button>
             </nav>
+        </>
     )
 }
