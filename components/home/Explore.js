4762c08c9512d58640745745edbf2891df03a02e
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-19 15:32:39 +0200

    change images and format code

--- components/home/Explore.js
+++ components/home/Explore.js
@@ -8,18 +8,37 @@ export default function Explore() {
             <Heading size="2" title="Explore bergen" />
             <CardsGrid>
                 <>
-                    <Card src="/img/natours.JPG" alt="bergen" title="Fløybanen" content="Lorem ipsum dolor sit amet, consectetur 
-                        adipiscing elit, sed do eiusmod tempor 
-                        incididunt ut labore et dolore magna 
-                        aliqua. Ut enim ad minim veniam, quis" link="https://www.visitbergen.com/" text="learn more" />
-                    <Card src="/img/natours.JPG" alt="bergen" title="Fløven" content="Lorem ipsum dolor sit amet, consectetur 
-                        adipiscing elit, sed do eiusmod tempor 
-                        incididunt ut labore." link="https://www.visitbergen.com/" text="learn more" />
-                    <Card src="/img/natours.JPG" alt="bergen" title="Bergen brygge" content="Lorem ipsum dolor sit amet, consectetur 
-                        adipiscing elit, sed do eiusmod tempor 
-                        incididunt ut labore et dolore magna" link="https://www.visitbergen.com/" text="learn more" />
-                    <Card src="/img/natours.JPG" alt="bergen" title="Akvarium" content="Lorem ipsum dolor sit amet, consectetur 
-                        adipiscing elit, sed do eiusmod tempor" link="https://www.visitbergen.com/" text="learn more" />
+                    <Card src="/img/bea-fladstad-3IN0hvhUCiY-unsplash.jpg" 
+                        alt="Fløybanen" 
+                        title="Fløybanen" 
+                        content="Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna 
+                            aliqua. Ut enim ad minim veniam, quis" 
+                        link="https://www.visitbergen.com/" 
+                        text="learn more" />
+                    <Card src="/img/sharon-christina-rorvik-fwcnv0WRsTY-unsplash.jpg" 
+                        alt="Fløven" 
+                        title="Fløven" 
+                        content="Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore." 
+                        link="https://www.visitbergen.com/" 
+                        text="learn more" />
+                    <Card src="/img/lachlan-gowen-AG3EcbcQOjw-unsplash.jpg" 
+                        alt="Bergen brygge" title="Bergen brygge" 
+                        content="Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna" 
+                        link="https://www.visitbergen.com/" 
+                        text="learn more" />
+                    <Card src="/img/giachen-s-world-UcFzZLmQcW0-unsplash.jpg" 
+                        alt="Akvariumet" 
+                        title="Akvarium" 
+                        content="Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit, sed do eiusmod tempor" 
+                        link="https://www.visitbergen.com/" 
+                        text="learn more" />
                 </>
             </ CardsGrid>
         </section>
