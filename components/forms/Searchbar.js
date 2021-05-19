5c7a9a0eb2ceb57932381477e63966939a63e21e
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-19 14:54:28 +0200

    add classnames and placeholder

--- components/forms/Searchbar.js
+++ components/forms/Searchbar.js
@@ -1,19 +1,14 @@
-import PropTypes from 'prop-types';
 import styles from './Searchbar.module.css';
 
-export default function Searchbar({ type, text }) {
+export default function Searchbar() {
+
     return (
         <form className={styles.form}>
-            <input
-                type={ type }
-                placeholder={ text }
+            <input 
+                type="text"
+                placeholder="search.."
                 className={styles.input}
             />
         </form>
-    )
+    );
 }
-
-Searchbar.propTypes = {
-	type: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
-};
