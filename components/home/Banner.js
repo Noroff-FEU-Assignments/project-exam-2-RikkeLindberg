8dd6d11719d74b9816f102093db3e4d0b0ff6b55
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-19 14:57:25 +0200

    format code

--- components/home/Banner.js
+++ components/home/Banner.js
@@ -1,6 +1,6 @@
-import Hero from './Hero';
-import styles from './Banner.module.css';
-import Searchbar from '../forms/Searchbar';
+import Hero from './Hero'
+import styles from './Banner.module.css'
+import Searchbar from '../forms/Searchbar'
 
 export default function Banner() {
     return (
