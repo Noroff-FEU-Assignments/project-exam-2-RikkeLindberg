4a2c9d97b6e5f338371b17af455b76d2d5472dc2
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-19 15:02:57 +0200

    import error page

--- pages/createNew.js
+++ pages/createNew.js
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import AddEstablishment from '../components/admin/AddEstablishment';
 import Heading from '../components/typography/Heading'
 import AuthContext from '../context/AuthContext'
+import FourOhFour from './404';
 
 export default function createNew() {
     const [auth] = useContext(AuthContext);
@@ -13,7 +14,9 @@ export default function createNew() {
                     <Heading size="1" title="Create new"/>
                     <AddEstablishment />
                 </div>
-                ) : ( <div>NOT ALLOWED</div> )
+                ) : ( 
+                <FourOhFour />
+                )
             }
         </div>
     )
