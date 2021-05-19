813a3d5a20926d769f2cc72084219743643eba9a
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-19 15:01:00 +0200

    import error page and add logout button

--- pages/admin.js
+++ pages/admin.js
@@ -1,24 +1,35 @@
 import { useContext } from "react";
+import { useRouter } from 'next/router'
 import Heading from '../components/typography/Heading'
 import Enquiries from '../components/admin/Enquiries'
 import Messages from '../components/admin/Messages'
 import Button from '../components/ui/Button'
 import AuthContext from "../context/AuthContext";
+import FourOhFour from "./404";
 
 export default function Admin() {
+    const [auth, setAuth] = useContext(AuthContext);
 
-    const [auth] = useContext(AuthContext);
+    const router = useRouter();
+
+    function logout() {
+        setAuth(null);
+        router.push("/login");
+    }
 
     return (
         <>
             { auth ? (
                 <div>
                     <Heading size="1" title="Admin"/>
+                    <button onClick={logout}>Log out</button>
                     <Button link="/createNew">Create New</Button>
                     <Messages />
                     <Enquiries />
                 </div>
-                ) : ( <div>NOT ALLOWED</div> )
+            ) : ( 
+                <FourOhFour /> 
+                )
             }
         </>
     )
