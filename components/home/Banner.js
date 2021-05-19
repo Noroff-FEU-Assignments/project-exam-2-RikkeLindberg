7012bca2c5aa97148d024828afb266bec90b5d46
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-19 14:56:54 +0200

    move Searchbar comp. to top

--- components/home/Banner.js
+++ components/home/Banner.js
@@ -5,8 +5,8 @@ import Searchbar from '../forms/Searchbar';
 export default function Banner() {
     return (
         <div className={styles.section}>
-                    <Hero className={styles.hero} />
                     <Searchbar />
+                    <Hero className={styles.hero} />
         </div>
     )
 }
