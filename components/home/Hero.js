f03061fdc935812e0c9b381ec28fb970566cbdf0
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-20 08:09:34 +0200

    format code

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -1,13 +1,15 @@
-import Button from '../ui/Button';
-import Heading from '../typography/Heading';
-import Paragraph from '../typography/Paragraph';
-import styles from './Hero.module.css';
+import Button from '../ui/Button'
+import Heading from '../typography/Heading'
+import Paragraph from '../typography/Paragraph'
+import styles from './Hero.module.css'
 
 export default function Hero() {
     return (
         <div className={styles.hero}>
             <Heading size="1" title="Tourism Agency in Bergen"/>
-            <Paragraph content="Visit Bergen and find the way you want to live on vacation either it is in a hotel, B&B or a guesthouse we got you covered."/>
+            <Paragraph content="Visit Bergen and find the way you 
+                                want to live on vacation either it is in a hotel, 
+                                B&B or a guesthouse we got you covered."/>
             <Button link="/hotels">See all hotels</Button>
         </div>
     )
