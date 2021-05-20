44eb35bbe43731fa034a0a0ebaf0b60cb2036d01
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-20 09:07:12 +0200

    create contact info component

---
+++ components/contact/ContactInfo.js
@@ -0,0 +1,25 @@
+import { BiEnvelope, BiPhone, BiMap } from 'react-icons/bi';
+import Link from 'next/link'
+import styles from './ContactInfo.module.css'
+
+export default function ContactInfo() {
+    return (
+        <div className={styles.container}>
+            <Link href="tel:5551234567">
+                <a>
+                    <BiPhone className={styles.icon} />
+                </a>
+            </Link>
+            <Link href="mailto:webmaster@example.com">
+                <a>
+                    <BiEnvelope className={styles.icon} />
+                </a>
+            </Link>
+            <Link href="https://goo.gl/maps/TsNsTERbUb976YxN6">
+                <a target="_blank">
+                    <BiMap className={styles.icon} />
+                </a>
+            </Link>  
+        </div>
+    )
+}
