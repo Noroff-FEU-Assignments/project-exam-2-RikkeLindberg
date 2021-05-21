37f55cbb74fc5bb3c73f6f10c70c07b5d029a8ac
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-21 21:09:43 +0200

    remove heading component

--- pages/createNew.js
+++ pages/createNew.js
@@ -1,6 +1,5 @@
 import { useContext } from 'react'
 import AddEstablishment from '../components/admin/AddEstablishment';
-import Heading from '../components/typography/Heading'
 import AuthContext from '../context/AuthContext'
 import FourOhFour from './404';
 
@@ -8,16 +7,13 @@ export default function createNew() {
     const [auth] = useContext(AuthContext);
 
     return (
-        <div>
+        <section>
             { auth ? (
-                <div>
-                    <Heading size="1" title="Create new"/>
-                    <AddEstablishment />
-                </div>
+                <AddEstablishment />
                 ) : ( 
                 <FourOhFour />
                 )
             }
-        </div>
+        </section>
     )
 }
