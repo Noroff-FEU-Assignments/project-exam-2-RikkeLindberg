8f4950b1605f947a9280caae53050e46ae144a01
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-21 20:54:39 +0200

    format code

--- pages/enquiry/[slug].js
+++ pages/enquiry/[slug].js
@@ -5,10 +5,11 @@ import { useRouter } from 'next/router'
 import Heading from '../../components/typography/Heading'
 import FormButton from '../../components/ui/FormButton'
 import { BASE_URL } from '../../constants/api'
+import styles from './enquiry.module.css'
 
 const url = BASE_URL + "enquiries";
 
-export default function Enquiry() {
+export default function Enquiry() { 
     const [posted, setPosted] = useState(false);
     const [submitting, setSubmitting] = useState(false);
 	const [postError, setPostError] = useState(null);
@@ -39,30 +40,28 @@ export default function Enquiry() {
 
     console.log({slug});
 
-    const slugName = slug.replace("-", " ");
-
     return (
         <section>
-            <Heading size="2" title="enquiry" />
-                <form onSubmit={handleSubmit(onSubmit)}>
-                { posted && <p>Thank you! Your enquiry was successfully submitted.</p> }
-			    { postError && <p>Sorry, something went wrong!</p> }
+
+            <div className={styles.container}>
+            <Heading size="1" title="enquiry" />
+
+                <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+                { posted && <p className={styles.success}>Thank you! Your enquiry was successfully submitted.</p> }
+			    { postError && <p className={styles.error}>Sorry, something went wrong!</p> }
 
                     <div>
                         <label htmlFor="establishment">Establishment:</label>
-                        { errors.establishment && errors.establishment.type === "required" && <p>This field is required</p> }
                         <input 
                             name="establishment"
-                            defaultValue={slugName}
-                            {...register('establishment', { 
-                                required: true,
-                            })} />
+                            defaultValue={slug}
+                            {...register('establishment')} />
                     </div>
 
 					<div>
                         <label htmlFor="name">Name:</label>
-                        { errors.name && errors.name.type === "required" && <p>This field is required</p> }
-                        { errors.name && errors.name.type === "minLength" && <p>Min length 3 characters</p> }
+                        { errors.name && errors.name.type === "required" && <p className={styles.error}>This field is required</p> }
+                        { errors.name && errors.name.type === "minLength" && <p className={styles.warning}>Min length 3 characters</p> }
 						<input 
 							name="name"
                             {...register('name', { 
@@ -74,8 +73,8 @@ export default function Enquiry() {
 
 					<div>
                         <label htmlFor="email">Email:</label>
-                        { errors.email && errors.email.type === "required" && <p>This field is required</p> }
-                        { errors.email && errors.email.type === "pattern" && <p>Must contain '@' and '.'</p> }
+                        { errors.email && errors.email.type === "required" && <p className={styles.error}>This field is required</p> }
+                        { errors.email && errors.email.type === "pattern" && <p className={styles.warning}>Must contain '@' and '.'</p> }
 						<input 
 							name="email" 
                             {...register('email', { 
@@ -87,7 +86,7 @@ export default function Enquiry() {
 
                     <div>
                         <label htmlFor="max_guests">Guests:</label>
-                        { errors.max_guests && errors.max_guests.type === "required" && <p>This field is required</p> }
+                        { errors.max_guests && errors.max_guests.type === "required" && <p className={styles.error}>This field is required</p> }
 						<select 
 							name="max_guests"
                             {...register('max_guests', { required: true })}
@@ -97,12 +96,16 @@ export default function Enquiry() {
                             <option value="2">2</option>
                             <option value="3">3</option>
                             <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
                         </select>
 					</div>
 
                     <div>
                         <label htmlFor="check_in">Check in:</label>
-                        { errors.check_in && errors.check_in.type === "required" && <p>This field is required</p> }
+                        { errors.check_in && errors.check_in.type === "required" && <p className={styles.error}>This field is required</p> }
 						<input 
 							name="check_in"
                             type="date"
@@ -111,7 +114,7 @@ export default function Enquiry() {
 
                     <div>
                         <label htmlFor="check_out">Check out:</label>
-                        { errors.check_out && errors.check_out.type === "required" && <p>This field is required</p> }
+                        { errors.check_out && errors.check_out.type === "required" && <p className={styles.error}>This field is required</p> }
 						<input 
 							name="check_out"
                             type="date"
@@ -120,8 +123,8 @@ export default function Enquiry() {
 
                     <div>
                         <label htmlFor="message">Message:</label>
-                        { errors.message && errors.message.type === "required" && <p>This field is required</p> }
-                        { errors.message && errors.message.type === "minLength" && <p>Min length 6 characters</p> }
+                        { errors.message && errors.message.type === "required" && <p className={styles.error}>This field is required</p> }
+                        { errors.message && errors.message.type === "minLength" && <p className={styles.warning}>Min length 6 characters</p> }
 						<textarea 
 							name="message" 
                             {...register('message', { 
@@ -131,8 +134,9 @@ export default function Enquiry() {
                         />
 					</div>
 
-					<FormButton disabled={ submitting }>submit</FormButton>
+					<FormButton type="submit" disabled={ submitting }>Submit</FormButton>
 			    </form>
+            </div>
         </section>
     );
 } 
