2aa715c83872e271314327ecdf026d35b217a2bd
RikkeLindberg <lindbergxrikke@gmail.com>
2021-05-21 11:27:21 +0200

    format code

--- components/home/Explore.js
+++ components/home/Explore.js
@@ -9,35 +9,36 @@ export default function Explore() {
             <CardsGrid>
                 <>
                     <Card src="/img/bea-fladstad-3IN0hvhUCiY-unsplash.jpg" 
-                        alt="Fløybanen" 
-                        title="Fløybanen" 
+                        alt="Fløibanen" 
+                        title="Fløibanen" 
                         content="Lorem ipsum dolor sit amet, consectetur 
                                 adipiscing elit, sed do eiusmod tempor 
                                 incididunt ut labore et dolore magna 
                                 aliqua. Ut enim ad minim veniam, quis" 
-                        link="https://www.visitbergen.com/" 
+                        link="https://www.floyen.no/floibanen/" 
                         text="learn more" />
                     <Card src="/img/sharon-christina-rorvik-fwcnv0WRsTY-unsplash.jpg" 
-                        alt="Fløven" 
-                        title="Fløven" 
+                        alt="Trolltunga" 
+                        title="Trolltunga" 
                         content="Lorem ipsum dolor sit amet, consectetur 
                                 adipiscing elit, sed do eiusmod tempor 
                                 incididunt ut labore." 
-                        link="https://www.visitbergen.com/" 
-                        text="learn more" />
+                        link="https://www.visitbergen.com/ting-a-gjore/guidet-overnattingstur-til-trolltunga-p5734853" 
+                        text="learn more" 
+                        />
                     <Card src="/img/lachlan-gowen-AG3EcbcQOjw-unsplash.jpg" 
                         alt="Bergen brygge" title="Bergen brygge" 
                         content="Lorem ipsum dolor sit amet, consectetur 
                                 adipiscing elit, sed do eiusmod tempor 
                                 incididunt ut labore et dolore magna" 
-                        link="https://www.visitbergen.com/" 
+                        link="https://www.visitbergen.com/ting-a-gjore/bryggen-i-bergen-p878553" 
                         text="learn more" />
                     <Card src="/img/giachen-s-world-UcFzZLmQcW0-unsplash.jpg" 
-                        alt="Akvariumet" 
-                        title="Akvarium" 
+                        alt="Akvariet" 
+                        title="Akvariet" 
                         content="Lorem ipsum dolor sit amet, consectetur 
                                 adipiscing elit, sed do eiusmod tempor" 
-                        link="https://www.visitbergen.com/" 
+                        link="https://www.akvariet.no/" 
                         text="learn more" />
                 </>
             </ CardsGrid>
